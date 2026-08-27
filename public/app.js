57f9daff502836571e1db7a04448211edc1a92af
arnaldovirtusconsult <arnaldo.virtusconsult@gmail.com>
2026-08-27 10:59:06 -0300

    aponta app.js para mario

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 /// Troque aqui pelo id do cliente cadastrado em config/clients.json no backend
-const CLIENTE_ID = 'coinbra';
+const CLIENTE_ID = 'mario';
 
 async function fetchContaAzulData(params = {}) {
   try {
